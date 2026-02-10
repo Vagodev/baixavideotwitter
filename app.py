a64a26c2ae5b12b5cea00cf16e7da6490dafb6bd
Vagodev <oi@vagodev.com>
2026-02-09 22:54:18 -0300

    Atualiza app.py para suportar Render

--- app.py
+++ app.py
@@ -17,5 +17,4 @@ def index():
             return render_template("index.html", result=f"Erro: {e}")
     return render_template("index.html", result=None)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": port = int(os.environ.get("PORT", 5000)) app.run(host="0.0.0.0", port=port)
